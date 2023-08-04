88e242b1dc2df9a8afadebe23eef86b617dc969d
rahulvit2010 <rahulvit2010@gmail.com>
2023-08-04 09:57:12 +0530

    LeetCode:792. Number of Matching Subsequences

---
+++ src/com/rahul/ds/practice/matchingsubsequence/Solution.java
@@ -0,0 +1,75 @@
+package com.rahul.ds.practice.matchingsubsequence;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.text.EditorKit;
+
+class Node
+{
+  String word;
+  int index;
+
+  public Node(String word, int index) {
+    this.word = word;
+    this.index = index;
+  }
+
+  public String getWord() {
+    return word;
+  }
+
+  public int getIndex() {
+    return index;
+  }
+}
+public class Solution {
+
+  public static void main(String[] args) {
+    String input="abcde";
+    String [] words= new String[]{"a","bb","acd","ace"};
+    int result=numMatchingSubseq(input,words);
+    System.out.println("result = " + result);
+  }
+
+  public static int numMatchingSubseq(String s, String[] words) {
+    ArrayList<Node>[] dictonary= new ArrayList[26];
+    int result=0;
+    // now place the words on the index in dictonary
+    for(int i=0;i< words.length;i++)
+    {
+      if(dictonary[words[i].charAt(0)-'a']==null)
+      {
+        dictonary[words[i].charAt(0)-'a']= new ArrayList<Node>();
+      }
+      dictonary[words[i].charAt(0)-'a'].add(new Node(words[i],0));
+    }
+
+    char[] sequence=s.toCharArray();
+    for(int j=0;j<sequence.length;j++)
+    {
+      if(dictonary[sequence[j]-'a']!=null)
+      {
+        List<Node>  prev_bucket= dictonary[sequence[j]-'a'];
+        dictonary[sequence[j]-'a']= new ArrayList<>();
+        for(int k=0;k<prev_bucket.size();k++)
+        {
+          Node node= prev_bucket.get(k);
+          int nextIndex=node.index+1;
+          if(nextIndex== node.word.length())
+          {
+            result++;
+          }
+          else
+          {
+            if(dictonary[node.word.charAt(nextIndex)-'a']==null)
+              dictonary[node.word.charAt(nextIndex)-'a']= new ArrayList<>();
+            dictonary[node.word.charAt(nextIndex)-'a'].add(new Node(node.word,nextIndex));
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+}
