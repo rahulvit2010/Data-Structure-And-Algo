fd28dfe96ea540d5198e1fa01aa6471f5b9a4fe7
rahulvit2010 <rahulvit2010@gmail.com>
2023-08-16 22:36:50 +0530

    linkedlist:cycle length+ first node of the cycle

--- src/com/rahul/ds/practice/linkedlist/LinkedListCycle/LinkedListCycle.java
+++ src/com/rahul/ds/practice/linkedlist/LinkedListCycle/LinkedListCycle.java
@@ -6,13 +6,34 @@ import com.rahul.ds.practice.linkedlist.impl.LinkedList;
 public class LinkedListCycle {
 
   public static void main(String[] args) {
-    LinkedList list = new LinkedList();
-    executeFirstTestCase(list);
-    executeSecondTestCase(list);
+    executeFirstTestCase();
+    executeSecondTestCase();
+    executeThirdTestCase();
+    executeFourthTestCase();
 
   }
 
-  private static void executeSecondTestCase(LinkedList list) {
+  private static void executeFourthTestCase() {
+    LinkedList list = new LinkedList();
+    createLinkedListWithCycle(list);
+    System.out.println("finding the length of the cycle");
+    int length = getlengthOfCycle(list);
+    System.out.println("length of the cycle is "+length);
+  }
+
+  private static void executeThirdTestCase() {
+    LinkedList list = new LinkedList();
+    createLinkedListWithCycle(list);
+    System.out.println("finding the first node of the cycle");
+    Node node = getFirstNodeOfCycle(list);
+    if(node==null)
+      System.out.println("there is no cycle");
+    else
+      System.out.println("First Node of the cycle is "+node.getValue());
+  }
+
+  private static void executeSecondTestCase() {
+    LinkedList list= new LinkedList();
     createLinkedListWitOutCycle(list);
     boolean result=hasCycle(list.getHead());
     System.out.println("LikedList Has Cycle:: "+result);
@@ -27,10 +48,69 @@ public class LinkedListCycle {
     list.push(87);
   }
 
-  private static void executeFirstTestCase(LinkedList list) {
+  private static void executeFirstTestCase() {
+    LinkedList list = new LinkedList();
     createLinkedListWithCycle(list);
     boolean result=hasCycle(list.getHead());
     System.out.println("LikedList Has Cycle:: "+result);
+    System.out.println("finding the first node of the cycle");
+    Node node = getFirstNodeOfCycle(list);
+    if(node==null)
+      System.out.println("there is no cycle");
+    else
+      System.out.println("First Node of the cycle is "+node.getValue());
+
+  }
+
+  private static Node getFirstNodeOfCycle(LinkedList list){
+    Node head= list.getHead();
+    Node slow= head;
+    Node fast= head;
+
+    while(fast!=null && fast.getNext()!=null)
+    {
+      fast= fast.getNext().getNext();
+      slow= slow.getNext();
+      if (slow==fast)
+        break;
+    }
+
+    if(slow!=fast)
+      return null;
+
+    slow=head;
+    while(slow !=fast)
+    {
+      slow=slow.getNext();
+      fast=fast.getNext();
+    }
+    return slow;
+  }
+
+  private static int getlengthOfCycle(LinkedList list){
+    Node head= list.getHead();
+    Node slow= head;
+    Node fast= head;
+
+    while(fast!=null && fast.getNext()!=null)
+    {
+      fast= fast.getNext().getNext();
+      slow= slow.getNext();
+      if (slow==fast)
+        break;
+    }
+
+    if(slow!=fast)
+      return 0;
+
+    int length=1;
+    Node temp=slow;
+    while(temp.getNext()!=slow)
+    {
+      length++;
+      temp=temp.getNext();
+    }
+    return length;
   }
 
   private static void createLinkedListWithCycle(LinkedList list) {
@@ -40,7 +120,7 @@ public class LinkedListCycle {
     list.push(5);
     list.push(90);
     list.push(87);
-    Node node=list.get(2);
+    Node node=list.get(0);
     list.getTail().setNext(node);
   }
 
